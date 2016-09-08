b8433e014cc6df313ea3302e2f7e1c6cdd3f12bd
Tim Garibaldi <tgaribal@gmail.com>
2016-09-07 17:05:20 -0700

    first pass at getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
+// Implement getElementsByClassName with your own function in src/getElementsByClassName.js, and make the specs pass.
+// You should use document.body, element.childNodes, and element.classList
+
+var getElementsByClassName = function(className) {
   // your code here
+  var results = [];
+  function checkNode(node) {
+  	//console.log(node.className)
+  	if (node.className.includes(className)) {
+  		results.push(node);
+  	}
+
+  	for (var i = 0; i < node.children.length; i++) {
+  		checkNode(node.children[i])
+  	}
+  }
+  checkNode(document.body)
+  return results;
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,31 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+  	return "null";
+
+  } else if (typeof obj === "number" || typeof obj === "boolean") {
+  	return obj.toString();
+
+  } else if (typeof obj === "string") {
+  	return '"'+obj+'"';
+
+  } else if (Array.isArray(obj)) {
+  	var results = []
+  	for (var i = 0; i < obj.length; i++) {
+  		results.push (stringifyJSON(obj[i]))
+  	}
+  	return '['+results+']';
+
+  } else if (typeof obj === 'object') {
+  	var results = [];
+  	for (var key in obj) {
+  		if (typeof obj[key] === 'function') {
+  			return '{}'
+  		} else {
+  			results.push(stringifyJSON(key) + ':' +stringifyJSON(obj[key]));
+  		}	
+  	}
+  	return '{'+results+'}';
+  }
 };
